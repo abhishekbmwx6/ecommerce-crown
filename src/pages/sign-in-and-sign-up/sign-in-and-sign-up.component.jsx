55a028d5b6e9db94b7849e7c593f2df66810a542
Abhishek <abhishek.dubey@accionlabs.com>
2020-10-16 16:11:01 +0530

    feat(email signin and signup)

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import SingIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.styles.scss'
 
 const SignInAndSignUpPage = (props) => (
-    < dic className="sign-in-and-sign-up" >
+    < div className="sign-in-and-sign-up" >
         <SingIn></SingIn>
-    </dic >
+        <SignUp></SignUp>
+    </div >
 )
 
 export default SignInAndSignUpPage
